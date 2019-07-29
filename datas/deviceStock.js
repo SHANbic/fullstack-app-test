5d85eefd03e6b208beb2a62a4556722dce57ad33
Pierre Lambert <pierre.lam91@gmail.com>
2019-07-29 10:06:36 +0200

    [  FEATURE ] Wrote some fake datas

---
+++ datas/deviceStock.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {
+    id: '0125850',
+    type: 'PS4',
+    history: [
+      { month: 'July', stock: 2567 },
+      { month: 'June', stock: 2212 },
+      { month: 'May', stock: 1910 }
+    ]
+  },
+  {
+    id: '0125886',
+    type: 'Xbox One',
+    history: [
+      { month: 'July', stock: 1599 },
+      { month: 'June', stock: 2053 },
+      { month: 'May', stock: 2141 }
+    ]
+  },
+  {
+    id: '0126164',
+    type: 'Nintendo Switch',
+    history: [
+      { month: 'July', stock: 2767 },
+      { month: 'June', stock: 1489 },
+      { month: 'May', stock: 905 }
+    ]
+  }
+];
