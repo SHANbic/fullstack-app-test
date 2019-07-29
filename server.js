8fc7b4e9de17dfbc2a284ebe9b0f9cc6707aa069
Pierre Lambert <pierre.lam91@gmail.com>
2019-07-29 10:05:33 +0200

    [ FEATURE ] Set up a backend server to soon receive http GET requests from client

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+const hardware = require('./datas/deviceStock');
+
+app.get('/:hardware', (req, res) => {
+  const found = hardware.find(item => item.type === req.params.hardware);
+
+  res.send(found);
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
